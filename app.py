bc9626b01ce87b261b5b4fea8a418a94a66b3646
SoldierrBoy <ivan.tumchuk@icloud.com>
2025-10-22 17:44:03 +0300

    Add dynamic title and education section

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, flash, session, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
+# Секретний ключ потрібний для flash-повідомлень та сесій (додам пізніше)
+app.secret_key = 'super_secret_key_for_koroliuk'
+
 @app.route('/')
-def main():
-    return 'Hello, world!'
+def resume():
+
+    return render_template('resume.html', title="My Resume")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title="Contacts")
 
 if __name__ == '__main__':
     app.run(debug=True)
